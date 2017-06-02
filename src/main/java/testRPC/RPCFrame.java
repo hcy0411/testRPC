5ecbf671e2403ea3fa683083e63e817a5ed4a1f6
hanchunyang <hanchunyang@weidian.com>
2017-06-02 15:27:54 +0800

    update

--- src/main/java/testRPC/RPCFrame.java
+++ src/main/java/testRPC/RPCFrame.java
@@ -19,12 +19,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RPCFrame implements RPC {
 
     /**
-     * 存储接口与实现的对应关系
+     * 存储接口注册信息
      */
     private final Map<Class<?>, Object> implCache = new ConcurrentHashMap<Class<?>, Object>();
 
     /**
-     * 存储接口与代理的对应关系
+     * 存储接口代理信息
      */
     private final Map<Class<?>, Object> proxyCache = new ConcurrentHashMap<Class<?>, Object>();
 
@@ -39,41 +39,30 @@ public class RPCFrame implements RPC {
 
     public void expose() throws IOException {
         //暴露服务端口
-        @SuppressWarnings("resource") ServerSocket server = new ServerSocket(8888);
+        ServerSocket server = new ServerSocket(6666);
         while (true) {
             final Socket socket = server.accept();
             /*@formatter:off*/
-            new Thread(new Runnable() {
-                public void run() {
-                    ObjectInputStream input = null;
-                    ObjectOutputStream output = null;
-                    try {
-                        input = new ObjectInputStream(socket.getInputStream());
-                        output = new ObjectOutputStream(socket.getOutputStream());
-                        //获取请求数据 - 接收请求,并解码
-                        Class<?> inter = (Class<?>) input.readObject();
-                        String methodName = input.readUTF();
-                        Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
-                        Object[] args = (Object[]) input.readObject();
-                        //根据接口信息获得对应的接口实现 - Dispatcher
-                        Object instance = implCache.get(inter);
-                        //调用接口实现 - invoke
-                        Method method = instance.getClass().getMethod(methodName, parameterTypes);
-                        Object result = method.invoke(instance, args);
-                        //返回结果 - 编码,并返回结果
-                        output.writeObject(result);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    } finally {
-                        try {
-                            input.close();
-                            output.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+            new Thread(() -> {
+                try (
+                        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+                    //获取请求数据 - 接收请求,并解码
+                    Class<?> inter = (Class<?>) in.readObject();
+                    String methodName = in.readUTF();
+                    Class<?>[] parameterTypes = (Class<?>[]) in.readObject();
+                    Object[] args = (Object[]) in.readObject();
+                    //根据接口信息获得对应的接口实现 - Dispatcher
+                    Object instance = implCache.get(inter);
+                    //调用接口实现 - invoke
+                    Method method = instance.getClass().getMethod(methodName, parameterTypes);
+                    Object result = method.invoke(instance, args);
+                    //返回结果 - 编码,并返回结果
+                    out.writeObject(result);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-            }).run();
+            }).start();
             /*@formatter:on*/
         }
 
@@ -88,35 +77,24 @@ public class RPCFrame implements RPC {
 
         /*@formatter:off*/
         T proxy = (T) Proxy.newProxyInstance(Thread.currentThread()
-                .getContextClassLoader(), new Class<?>[]{inteface}, new InvocationHandler() {
-            @SuppressWarnings("resource")
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                Socket socket = new Socket(InetAddress.getLocalHost(), 8888);
-                ObjectInputStream input = null;
-                ObjectOutputStream output = null;
-                try {
-                    output = new ObjectOutputStream(socket.getOutputStream());
-                    input = new ObjectInputStream(socket.getInputStream());
-                    //发送请求数据 - 编码,并发送请求
-                    output.writeObject(inteface);
-                    output.writeUTF(method.getName());
-                    output.writeObject(method.getParameterTypes());
-                    output.writeObject(args);
-                    //接收结果 - 接受数据,并解码
-                    Object result = input.readObject();
+                        .getContextClassLoader(), new Class<?>[]{inteface},
+                (Object proxy1, Method method, Object[] args) -> {
+                    Socket socket = new Socket(InetAddress.getLocalHost(), 6666);
+                    try (
+                            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+                            ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
+                        //发送请求数据 - 编码,并发送请求
+                        output.writeObject(inteface);
+                        output.writeUTF(method.getName());
+                        output.writeObject(method.getParameterTypes());
+                        output.writeObject(args);
+                        //接收结果 - 接受数据,并解码
+                        Object result = input.readObject();
 
-                    return result;
-                } finally {
-                    try {
-                        output.close();
-                        input.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        return result;
                     }
                 }
-            }
-
-        });
+        );
         /*@formatter:on*/
         proxyCache.put(inteface, proxy);
         return proxy;
